1a45a08e68a90fae038557c8b9640249d17b84ed
Luiz Américo <luizamericop@gmail.com>
2018-04-15 20:45:11 -0300

    Add example of template for index/view

--- generators/app/templates/src/index/view.js
+++ generators/app/templates/src/index/view.js
@@ -1,5 +1,7 @@
 import {View} from 'backbone.marionette'
 
 export default View.extend({
-
+  template: '<h1>Hello World</h1>'
+  // or JSX
+  //template: function() {return <h1>Hello World</h1>}
 })
